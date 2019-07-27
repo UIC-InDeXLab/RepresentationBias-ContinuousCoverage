51da8a0c9d7c8d4dc4f38fcebc6b7d6bc55ba99a
Zhongjun Jin <markjin1990@gmail.com>
2019-07-27 09:26:58 -0400

    First commit

---
+++ src/main/java/umichdb/coverage2/App.java
@@ -0,0 +1,13 @@
+package umichdb.coverage2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
