35f40a97e970f5d9a2dcc5326b1d7f11e574ad0e
Zhongjun Jin <markjin1990@gmail.com>
2019-11-28 02:28:27 -0500

    try to modify an existing high-order voronoi package

--- src/main/java/umichdb/coverage2/Draw.java
+++ src/main/java/umichdb/coverage2/Draw.java
@@ -1,11 +1,16 @@
 package umichdb.coverage2;
 
 import java.awt.Color;
+import java.awt.geom.Ellipse2D;
+
 import javax.swing.JFrame;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.annotations.XYBoxAnnotation;
+import org.jfree.chart.annotations.XYShapeAnnotation;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.xy.XYDataset;
@@ -16,7 +21,7 @@ import org.jfree.data.xy.XYSeriesCollection;
 public class Draw  extends JFrame {
 	  private static final long serialVersionUID = 6294689542092367723L;
 
-	  public Draw(String title, NDPoint[] points, NDCube cube) {
+	  public Draw(String title, NDPoint[] points, double radius) {
 	    super(title);
 
 	    // Create dataset
@@ -24,11 +29,22 @@ public class Draw  extends JFrame {
 
 	    // Create chart
 	    JFreeChart chart = ChartFactory.createScatterPlot("coverage", "x", "y", pointsSet, PlotOrientation.HORIZONTAL, false, false, false);
-	    
+
 	    //Changes background color
 	    XYPlot plot = (XYPlot)chart.getPlot();
 	    plot.setBackgroundPaint(new Color(255,228,196));
-	    plot.addAnnotation(new XYBoxAnnotation( cube.getMinValAtDim(0), cube.getMinValAtDim(1), cube.getMaxValAtDim(0), cube.getMaxValAtDim(1)));
+	    
+	    ValueAxis domainAxis = plot.getDomainAxis();
+	    ValueAxis rangeAxis = plot.getRangeAxis();
+
+	    domainAxis.setRange(0.0, 1.0);
+	    rangeAxis.setRange(0.0, 1.0);
+	    
+	    for (NDPoint p : points) {
+		    plot.addAnnotation(new XYShapeAnnotation(new Ellipse2D.Double(p.valAtNDimension(0) - radius, p.valAtNDimension(1) - radius, radius + radius, radius + radius)));
+	    }
+
+//	    plot.addAnnotation(new XYBoxAnnotation( cube.getMinValAtDim(0), cube.getMinValAtDim(1), cube.getMaxValAtDim(0), cube.getMaxValAtDim(1)));
 	      
 	    // Create Panel
 	    ChartPanel panel = new ChartPanel(chart);
--- src/main/java/umichdb/coverage2/FindCoverageContinuous.java
+++ src/main/java/umichdb/coverage2/FindCoverageContinuous.java
@@ -16,166 +16,7 @@ import java.util.List;
 import org.apache.commons.math3.util.*;
 
 public class FindCoverageContinuous {
-	/**
-	 * A naive approach to discover the uncovered space (N-dimensional cube).
-	 * 
-	 * @param points
-	 * @return
-	 */
-	public static NDCube findMaxVolumeNDCubeNaive(NDPoint[] points) {
-		NDCube NDCubeMaxVolume = null;
 
-		if (points == null || points.length < 1) {
-			return null;
-		}
-
-		int dim = points[0].getDimensions();
-
-		// not enough points to formulate a least one hypercube
-		if (points.length < 2) {
-			return null;
-		}
-
-		// Enumerate all possible hypercubes. Note that each n-dimensional cube
-		// (hyper cube is uniquely defined by 2...2n points.
-		for (int numVertices = 2; numVertices <= 2 * dim; numVertices++) {
-			if (numVertices > points.length) {
-				break;
-			}
-
-			Iterator<int[]> it = CombinatoricsUtils
-					.combinationsIterator(points.length, numVertices);
-
-			while (it.hasNext()) {
-				// new set of vertices
-				int[] indexes = it.next();
-				NDPoint[] tempPoints = new NDPoint[indexes.length];
-				for (int i = 0; i < indexes.length; i++) {
-					tempPoints[i] = points[indexes[i]];
-				}
-
-				// hypercube defined by 2d points
-				NDCube newCube = createNDCubeByNDPoints(tempPoints);
-
-				if (newCube == null) {
-					continue;
-				}
-
-				// Check if the hypercube is totally empty.
-				Set<Integer> indexSet = new HashSet<Integer>();
-				for (int idx : indexes) {
-					indexSet.add(idx);
-				}
-
-				boolean ifEmptyCube = true;
-				for (int i = 0; i < points.length; i++) {
-					if (!indexSet.contains(i)) {
-						NDPoint otherPoint = points[i];
-						if (newCube.contains(otherPoint)) {
-							ifEmptyCube = false;
-							break;
-						}
-					}
-				}
-
-				// Check if it has max volume
-				if (ifEmptyCube && (NDCubeMaxVolume == null
-						|| newCube.getVolume() > NDCubeMaxVolume.getVolume())) {
-					NDCubeMaxVolume = newCube;
-				}
-			}
-		}
-
-		// Return the one with the largest volume
-		return NDCubeMaxVolume;
-	}
-
-	/**
-	 * Proposed approach to discover the uncovered space
-	 * @param points
-	 * @return
-	 */
-	public static NDCube findMaxVolumeNDCube(NDPoint[] points) {
-		NDCube NDCubeMaxVolume = null;
-
-		if (points == null || points.length < 1) {
-			return null;
-		}
-
-		int d = points[0].getDimensions();
-
-		// not enough points to formulate a least one hypercube
-		if (points.length < 2) {
-			return null;
-		}
-
-		// Enumerate all possible pairs of points
-		Iterator<int[]> it = CombinatoricsUtils
-				.combinationsIterator(points.length, 2);
-
-		while (it.hasNext()) {
-			// new set of vertices
-			int[] indexes = it.next();
-			NDPoint[] tempPoints = new NDPoint[indexes.length];
-			for (int i = 0; i < indexes.length; i++) {
-				tempPoints[i] = points[indexes[i]];
-			}
-		}
-		
-		// Return the one with the largest volume
-		return NDCubeMaxVolume;
-	}
-
-	private static NDCube createNDCubeByNDPoints(NDPoint[] points) {
-		if (points == null || points.length < 1) {
-			return null;
-		}
-
-		int dim = points[0].getDimensions();
-
-		// Dimensionality is wrong return null. The number of points < 2.
-		if (points.length < 2) {
-			return null;
-		}
-
-		NDCube newCube = new NDCube(dim);
-
-		for (int d = 0; d < dim; d++) {
-			double maxValAtDim = Double.MIN_VALUE;
-			double minValAtDim = Double.MAX_VALUE;
-
-			for (NDPoint p : points) {
-				double valAtDim = p.valAtNDimension(d);
-				if (valAtDim > maxValAtDim) {
-					maxValAtDim = valAtDim;
-				}
-				if (valAtDim < minValAtDim) {
-					minValAtDim = valAtDim;
-				}
-				newCube.setMinValAtDim(d, minValAtDim);
-				newCube.setMaxValAtDim(d, maxValAtDim);
-			}
-		}
-
-		// Check if every point is on the edge. If not, this is not a good cube,
-		// we ignore it.
-		for (NDPoint p : points) {
-			boolean ifOnEdge = false;
-			for (int d = 0; d < dim; d++) {
-				if (p.valAtNDimension(d) <= newCube.getMinValAtDim(d)
-						|| p.valAtNDimension(d) >= newCube.getMaxValAtDim(d)) {
-					ifOnEdge = true;
-					break;
-				}
-			}
-			if (!ifOnEdge) {
-				return null;
-			}
-		}
-
-		return newCube;
-
-	}
 
 	/**
 	 * Randomly generate n d-dimensional points
@@ -206,12 +47,12 @@ public class FindCoverageContinuous {
 		return randPoints;
 	}
 
-	public static void visualize(NDPoint[] points, NDCube coverageCube) {
+	public static void visualize(NDPoint[] points, double radius) {
 		SwingUtilities.invokeLater(() -> {
 			Draw example = new Draw(
 					"Find uncovered space for " + points.length + " random points in "
 							+ points[0].getDimensions() + "-d space.",
-					points, coverageCube);
+					points, radius);
 			example.setSize(800, 800);
 			example.setLocationRelativeTo(null);
 			example.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -220,8 +61,11 @@ public class FindCoverageContinuous {
 	}
 
 	public static void main(String[] args) {
-		int n = 50;
+		int n = 10;
 		int d = 2;
+		double theta = 0.1;
+		int k = 5;
+		
 		System.out.printf("Create %d cubes of dimension %d\n\t", n, d);
 		NDPoint[] randPoints = genRandNDPoint(n, d);
 		for (NDPoint p : randPoints) {
@@ -232,13 +76,13 @@ public class FindCoverageContinuous {
 		System.out.println(
 				"Start finding coverage cube using the naive approach");
 		double beginTime = System.currentTimeMillis();
-		NDCube maxCube = findMaxVolumeNDCubeNaive(randPoints);
+		
 		double endTime = System.currentTimeMillis();
 
 		System.out.println("Cube found!");
-		System.out.println(maxCube);
+//		System.out.println(maxCube);
 		System.out.printf("Search time: %f ms", endTime - beginTime);
 
-		visualize(randPoints, maxCube);
+		visualize(randPoints, theta);
 	}
 }
